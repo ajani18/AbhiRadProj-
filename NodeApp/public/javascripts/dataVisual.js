14d9fe15cfa28e56b789b283de592dad0852f2f8
ajani18 <abhijani18@newmanschool.org>
2017-11-20 11:34:06 -0600

    H

--- NodeApp/public/javascripts/dataVisual.js
+++ NodeApp/public/javascripts/dataVisual.js
@@ -16,7 +16,6 @@ height = 270 - margin.top - margin.bottom;
 var parseDate = d3.utcParse("%Y-%m-%dT%H:%M:%S.%LZ");
 var bisectDate = d3.bisector(function (d) { return parseDate(d.timestamp); }).left;
 
-graphData();
 // getData();
 //
 // function getData() {
@@ -65,8 +64,9 @@ function graphTemp(data) {
   .y(function(d) { return y(d.temperature); });
 
   // Adds the svg canvas
-  var svg = d3.select("body")
+  var svg = d3.select("#chart")
   .append("svg")
+  .attr("id", "bigGraph")
   .attr("width", width + margin.left + margin.right)
   .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -141,112 +141,7 @@ function graphTemp(data) {
           i = bisectDate(data, x0, 1),
           d0 = data[i - 1],
           d1 = data[i],
-          d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-      focus.attr("transform", "translate(" + x(parseDate(d.timestamp)) + "," + y(d.temperature) + ")");
-      focus.select("text").text((d.temperature));
-  }
-
-  // });
-}
-
-function graphHumidity(data) {
-  var x = d3.time.scale().range([0, width]);
-  var y = d3.scale.linear().range([height, 0]);
-
-  // Define the axes
-  var xAxis = d3.svg.axis().scale(x)
-  .orient("bottom").ticks(5);
-
-  var yAxis = d3.svg.axis().scale(y)
-  .orient("left").ticks(5);
-
-  // Define the line
-  var valueline = d3.svg.line()
-  .x(function(d, i) {
-    // var date = new Date();
-    // console.log(date.getDate()  );
-    return x(parseDate(d.timestamp));
-  })
-  .y(function(d) { return y(d.humidity); });
-
-  // Adds the svg canvas
-  var svg = d3.select("body")
-  .append("svg")
-  .attr("width", width + margin.left + margin.right)
-  .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-  .attr("transform",
-  "translate(" + margin.left + "," + margin.top + ")");
-
-  // d3.csv("/javascripts/energy.csv", function(error, data) { //indentation?
-  //     data.forEach(function(d) {
-  //         d.date  = parseDate(d.date);
-  //         d.close = +d.close;
-  //     });
-  //
-  // Scale the range of the data
-  x.domain(d3.extent(data, function(d) {
-    return parseDate(d.timestamp); }));
-  y.domain([0, d3.max(data, function(d) { return d.humidity; })]);
-
-  // Add the valueline path.
-  svg.append("path")
-  .attr("class", "line")
-  .attr("d", valueline(data))
-  .attr("stroke", "#FF00FF")
-  .attr("fill", "none");
-
-  // Add the X Axis
-  svg.append("g")
-  .attr("class", "x axis")
-  .attr("transform", "translate(0," + height + ")")
-  .call(xAxis);
-
-  // Add the Y Axis
-  svg.append("g")
-  .attr("class", "y axis")
-  .call(yAxis);
-
-  svg.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 0 - margin.left)
-  .attr("x",0 - (height / 2))
-  .attr("dy", "1em")
-  .style("text-anchor", "middle")
-  .text("Humidity(%)");
-
-  svg.append("text")
-  .attr("transform",
-        "translate(" + (width/2) + " ," +
-                       (height + margin.top + 20) + ")")
-  .style("text-anchor", "middle")
-  .text("Date");
-
-  var focus = svg.append("g")
-      .attr("class", "focus")
-      .style("display", "none");
-
-  focus.append("circle")
-      .attr("r", 4.5);
-
-  focus.append("text")
-      .attr("x", 9)
-      .attr("dy", ".35em");
-
-  svg.append("rect")
-      .attr("class", "overlay")
-      .attr("width", width)
-      .attr("height", height)
-      .on("mouseover", function () { focus.style("display", null); })
-      .on("mouseout", function () { focus.style("display", "none"); })
-      .on("mousemove", mousemove);
-
-  function mousemove() {
-      var x0 = x.invert(d3.mouse(this)[0]),
-          i = bisectDate(data, x0, 1),
-          d0 = data[i - 1],
-          d1 = data[i],
-          d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+          d = x0 - d0.timestamp > d1.timestamp - x0 ? d1 : d0;
       focus.attr("transform", "translate(" + x(parseDate(d.timestamp)) + "," + y(d.temperature) + ")");
       focus.select("text").text((d.temperature));
   }
