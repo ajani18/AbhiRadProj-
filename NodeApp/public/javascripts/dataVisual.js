2d75748592d7fe3caa6306df5c3736f421c3019c
ajani18 <abhijani18@newmanschool.org>
2017-11-29 14:08:10 -0600

    Latest Commit

--- NodeApp/public/javascripts/dataVisual.js
+++ NodeApp/public/javascripts/dataVisual.js
@@ -3,6 +3,8 @@ var num = url.indexOf('classes/'); //get index of before classes
 var courseClass = url.slice(num + 8);
 console.log(courseClass);
 
+var storedData = [];
+
 queue()
   .defer(d3.json, "../classesdata/" + courseClass)
   .await(graphData);
@@ -36,13 +38,10 @@ var bisectDate = d3.bisector(function (d) { return parseDate(d.timestamp); }).le
 
 // Set the ranges
 function graphData(err, data){
-  graphTemp(data);
-  graphHumidity(data);
+  console.log(data.length);
+  storedData = data;
   // graphLight(data);
-}
-
-function graphTemp(data) {
-  // console.log(data);
+  // graphHumidity(data);
 
   var x = d3.time.scale().range([0, width]);
   var y = d3.scale.linear().range([height, 0]);
@@ -93,13 +92,13 @@ function graphTemp(data) {
 
   // Add the X Axis
   svg.append("g")
-  .attr("class", "x axis")
+  .attr("class", "xaxis")
   .attr("transform", "translate(0," + height + ")")
   .call(xAxis);
 
   // Add the Y Axis
   svg.append("g")
-  .attr("class", "y axis")
+  .attr("class", "yaxis")
   .call(yAxis);
 
   svg.append("text")
@@ -108,6 +107,7 @@ function graphTemp(data) {
   .attr("x",0 - (height / 2))
   .attr("dy", "1em")
   .style("text-anchor", "middle")
+  .attr("class", "yAxisLabel")
   .text("Temperature(F°)");
 
   svg.append("text")
@@ -149,4 +149,129 @@ function graphTemp(data) {
   // });
 }
 
+function graphTemp() {
+  // console.log(data);
+
+  data = storedData;
+
+  var x = d3.time.scale().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
+
+  // Define the axes
+  var xAxis = d3.svg.axis().scale(x)
+  .orient("bottom").ticks(5);
+
+  var yAxis = d3.svg.axis().scale(y)
+  .orient("left").ticks(5);
+
+  // Define the line
+  var valueline = d3.svg.line()
+  .x(function(d, i) {
+    // var date = new Date();
+    // console.log(date.getDate()  );
+    return x(parseDate(d.timestamp));
+  })
+  .y(function(d) { return y(d.temperature); });
+
+  // Adds the svg canvas
+  var svg = d3.select("#chart");
+  // d3.csv("/javascripts/energy.csv", function(error, data) { //indentation?
+  //     data.forEach(function(d) {
+  //         d.date  = parseDate(d.date);
+  //         d.close = +d.close;
+  //     });
+  //
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) {
+    return parseDate(d.timestamp); }));
+  y.domain([0, d3.max(data, function(d) { return d.temperature; })]);
+
+  // Add the valueline path.
+  svg.select("path.line")
+  .attr("d", valueline(data))
+
+  // Add the X Axis
+  svg.select(".xaxis")
+  .call(xAxis);
+
+  // Add the Y Axis
+  svg.select(".yaxis")
+  .call(yAxis);
+
+  svg.select(".yAxisLabel")
+  .text("Temperature(°F)");
+
+  // svg.append("text")
+  // .attr("transform",
+  //       "translate(" + (width/2) + " ," +
+  //                      (height + margin.top + 20) + ")")
+  // .style("text-anchor", "middle")
+  // .text("Date");
+
+}
+
+function graphHumidity() { 
+
+  data = storedData;
+
+  var x = d3.time.scale().range([0, width]);
+  var y = d3.scale.linear().range([height, 0]);
+
+  // Define the axes
+  var xAxis = d3.svg.axis().scale(x)
+  .orient("bottom").ticks(5);
+
+  var yAxis = d3.svg.axis().scale(y)
+  .orient("left").ticks(5);
+
+  // Define the line
+  var valueline = d3.svg.line()
+  .x(function(d, i) {
+    // var date = new Date();
+    // console.log(date.getDate()  );
+    return x(parseDate(d.timestamp));
+  })
+  .y(function(d) { return y(d.humidity); });
+
+  // Adds the svg canvas
+  var svg = d3.select("#chart");
+  // d3.csv("/javascripts/energy.csv", function(error, data) { //indentation?
+  //     data.forEach(function(d) {
+  //         d.date  = parseDate(d.date);
+  //         d.close = +d.close;
+  //     });
+  //
+  // Scale the range of the data
+  x.domain(d3.extent(data, function(d) {
+    return parseDate(d.timestamp); }));
+  y.domain([0, d3.max(data, function(d) { return d.humidity; })]);
+
+  // Add the valueline path.
+  svg.select("path.line")
+  .attr("d", valueline(data))
+
+  // Add the X Axis
+  svg.select(".xaxis")
+  .call(xAxis);
+
+  // Add the Y Axis
+  svg.select(".yaxis")
+  .call(yAxis);
+
+  svg.select(".yAxisLabel")
+  .text("Humidity(%)");
+
+  // svg.append("text")
+  // .attr("transform",
+  //       "translate(" + (width/2) + " ," +
+  //                      (height + margin.top + 20) + ")")
+  // .style("text-anchor", "middle")
+  // .text("Date");
+
+}
+
+function graphOneDay() {
+
+
+}
 
--- NodeApp/routes/classesData.js
+++ NodeApp/routes/classesData.js
@@ -11,7 +11,7 @@ router.get('/mrjones', function (req, res, next) {
     MongoClient.connect('mongodb://abhijani123:Beastmode17@cluster0-shard-00-00-8t9ca.mongodb.net:27017,cluster0-shard-00-01-8t9ca.mongodb.net:27017,cluster0-shard-00-02-8t9ca.mongodb.net:27017/RAD?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', function (err, db) {
             if (err) { return console.dir(err); }
             var collection = db.collection('MrJones');
-            collection.find().sort({ date: -1 }).toArray(function (err, dataJones) {
+            collection.find().sort({ timestamp: -1 }).toArray(function (err, dataJones) {
                 res.json(dataJones); //
             });
         });
@@ -21,6 +21,7 @@ router.get('/makerspace', function (req, res, next) {
      MongoClient.connect('mongodb://abhijani123:Beastmode17@cluster0-shard-00-00-8t9ca.mongodb.net:27017,cluster0-shard-00-01-8t9ca.mongodb.net:27017,cluster0-shard-00-02-8t9ca.mongodb.net:27017/testing?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', function (err, db) {
             if (err) { return console.dir(err); }
             var collection = db.collection('makerspace');
+            //new ISODate("2017-11-21T00:00:00Z") 
             collection.find().sort({ timestamp: -1 }).toArray(function (err, dataMakerSpace) { 
                 res.json(dataMakerSpace); 
             });
